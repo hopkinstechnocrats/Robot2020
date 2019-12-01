643ff87ac3da1912dd7cc3a35a874573eca83deb
Henry Scheible <henry.scheible@gmail.com>
2019-11-30 20:35:11 -0600

    write basic component abstract class

---
+++ src/main/java/frc/robot/components/Component.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.components;
+
+import frc.robot.logger.*;
+
+/**
+ * This is the abstract class defining the behavior of a hardware component, and classes defining each hardware component should extend it
+ */
+public abstract class Component {
+
+    public Status getStatus(){
+
+    }
+    
+}
