ea250ed477d1fb34e2e58526b5d81ad921de6151
Henry Scheible <henry.scheible@gmail.com>
2019-12-15 09:15:49 -0600

    Separate TankDriveTest into multiple test cases

--- src/test/java/frc/robot/subsystems/TankDriveTest.java
+++ src/test/java/frc/robot/subsystems/TankDriveTest.java
@@ -20,24 +20,117 @@ import frc.robot.subsystems.*;
  */
 public class TankDriveTest {
 
-    @Test
-    public void testConstructor() {
-        //Arrange
-        Talon mocktalonleft1 = mock(Talon.class);
-        Talon mocktalonleft2 = mock(Talon.class);
-        Talon mocktalonright1 = mock(Talon.class);
-        Talon mocktalonright2 = mock(Talon.class);
+    Talon mocktalonleft1;
+    Talon mocktalonleft2;
+    Talon mocktalonright1;
+    Talon mocktalonright2;
+    TankDrive drive;
 
-        //Act
-        TankDrive drive = new TankDrive(mocktalonleft1, mocktalonleft2, mocktalonright1, mocktalonright2);
-        for (double i=-1; i<1; i+=0.1){
-            drive.tankDrive(i, i, false);
-            System.out.println(i);
-            verify(mocktalonleft1).set((double)i);
-            verify(mocktalonleft2).set((double)i);
-            verify(mocktalonright1).set((double)-i);
-            verify(mocktalonright2).set((double)-i);
-        }
-        
+    @Before
+    public void setup() {
+        mocktalonleft1 = mock(Talon.class);
+        mocktalonleft2 = mock(Talon.class);
+        mocktalonright1 = mock(Talon.class);
+        mocktalonright2 = mock(Talon.class);
+        drive = new TankDrive(mocktalonleft1, mocktalonleft2, mocktalonright1, mocktalonright2);
+    }
+
+    @Test
+    public void testDriveFowardFull() {
+        double leftspeed = 1;
+        double rightspeed = 1;
+        drive.tankDrive(leftspeed, rightspeed, false);
+        verify(mocktalonleft1).set(leftspeed);
+        verify(mocktalonleft2).set(leftspeed);
+        verify(mocktalonright1).set(-rightspeed);
+        verify(mocktalonright2).set(-rightspeed);
+    }
+
+    @Test
+    public void testDriveForwardHalf() {
+        double leftspeed = 0.5;
+        double rightspeed = 0.5;
+        drive.tankDrive(leftspeed, rightspeed, false);
+        verify(mocktalonleft1).set(leftspeed);
+        verify(mocktalonleft2).set(leftspeed);
+        verify(mocktalonright1).set(-rightspeed);
+        verify(mocktalonright2).set(-rightspeed);
+    }
+
+    @Test
+    public void testDriveBackwardFull() {
+        double leftspeed = -1;
+        double rightspeed = -1;
+        drive.tankDrive(leftspeed, rightspeed, false);
+        verify(mocktalonleft1).set((double)leftspeed);
+        verify(mocktalonleft2).set((double)leftspeed);
+        verify(mocktalonright1).set((double)-rightspeed);
+        verify(mocktalonright2).set((double)-rightspeed);
+    }
+
+    @Test
+    public void testDriveBackwardHalf() {
+        double leftspeed = -0.5;
+        double rightspeed = -0.5;
+        drive.tankDrive(leftspeed, rightspeed, false);
+        verify(mocktalonleft1).set((double)leftspeed);
+        verify(mocktalonleft2).set((double)leftspeed);
+        verify(mocktalonright1).set((double)-rightspeed);
+        verify(mocktalonright2).set((double)-rightspeed);
+    }
+
+    @Test
+    public void testDriveStill() {
+        double leftspeed = 0;
+        double rightspeed = 0;
+        drive.tankDrive(leftspeed, rightspeed, false);
+        verify(mocktalonleft1).set((double)leftspeed);
+        verify(mocktalonleft2).set((double)leftspeed);
+        verify(mocktalonright1).set((double)-rightspeed);
+        verify(mocktalonright2).set((double)-rightspeed);
+    }
+
+    @Test
+    public void testTurnRightFull() {
+        double leftspeed = 1;
+        double rightspeed = -1;
+        drive.tankDrive(leftspeed, rightspeed, false);
+        verify(mocktalonleft1).set((double)leftspeed);
+        verify(mocktalonleft2).set((double)leftspeed);
+        verify(mocktalonright1).set((double)-rightspeed);
+        verify(mocktalonright2).set((double)-rightspeed);
+    }
+
+    @Test
+    public void testTurnLeftFull() {
+        double leftspeed = -1;
+        double rightspeed = 1;
+        drive.tankDrive(leftspeed, rightspeed, false);
+        verify(mocktalonleft1).set((double)leftspeed);
+        verify(mocktalonleft2).set((double)leftspeed);
+        verify(mocktalonright1).set((double)-rightspeed);
+        verify(mocktalonright2).set((double)-rightspeed);
+    }
+
+    @Test
+    public void testTurnRightHalf() {
+        double leftspeed = 0.5;
+        double rightspeed = -0.5;
+        drive.tankDrive(leftspeed, rightspeed, false);
+        verify(mocktalonleft1).set((double)leftspeed);
+        verify(mocktalonleft2).set((double)leftspeed);
+        verify(mocktalonright1).set((double)-rightspeed);
+        verify(mocktalonright2).set((double)-rightspeed);
+    }
+
+    @Test
+    public void testTurnLeftHalf() {
+        double leftspeed = -0.5;
+        double rightspeed = 0.5;
+        drive.tankDrive(leftspeed, rightspeed, false);
+        verify(mocktalonleft1).set((double)leftspeed);
+        verify(mocktalonleft2).set((double)leftspeed);
+        verify(mocktalonright1).set((double)-rightspeed);
+        verify(mocktalonright2).set((double)-rightspeed);
     }
 }
