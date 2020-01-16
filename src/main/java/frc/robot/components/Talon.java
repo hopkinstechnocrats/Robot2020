2e28e9aedd5039d26b017a9840c16090aeebac49
EliteBook 8770w <henry.scheible@gmail.com>
2020-01-16 15:22:15 -0600

    Start writing motion profile code

--- src/main/java/frc/robot/components/Talon.java
+++ src/main/java/frc/robot/components/Talon.java
@@ -1,13 +1,17 @@
 package frc.robot.components;
+import com.ctre.phoenix.motion.BufferedTrajectoryPointStream;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.logger.Status;
 import frc.robot.logger.StatusType;
 import edu.wpi.first.wpilibj.SpeedController;
+import java.util.*;
 
 public class Talon extends Component implements SpeedController{
 
     WPI_TalonSRX hardwareTalon;
+    HashMap<String, BufferedTrajectoryPointStream> motionProfiles = new HashMap<String, BufferedTrajectoryPointStream>();
+    
 
     public Talon(int id){
         hardwareTalon = new WPI_TalonSRX(id);
@@ -48,5 +52,15 @@ public class Talon extends Component implements SpeedController{
 
     public Status getStatus(){
         return new Status(StatusType.LOG, "Example Talon Log Message");
+    }
+
+    public boolean loadMotionProfile(String ProfilePath){
+        try{
+            
+        }
+        catch(Exception e){
+            return false;
+        }
+        return true;
     }
 }
---
+++ src/test/java/frc/robot/commands/DriveMotionProfileTest.java
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.TankDrive;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import edu.wpi.first.wpilibj.*;
+import org.junit.*;
+import frc.robot.Components;
+import frc.robot.components.Talon;
+import frc.robot.subsystems.*;
+import frc.robot.commands.DriveMotionProfile;
+
+/**
+ * Add your docs here.
+ */
+public class DriveMotionProfileTest {
+    
+    TankDrive subsystem;
+    DriveMotionProfile command;
+
+    public void setup() {
+        subsystem = mock(TankDrive.class);
+        command = new DriveMotionProfile();
+    }; 
+
+}
